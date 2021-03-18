2d8f705cb634a6288d621df17cabd778a129156e
Luis Henrique Alberti <luishenrialberti@gmail.com>
2021-03-18 17:52:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+var token = 'IGQVJVSzBVLU5TdUt2RWFJXzI5enZAZATkgxVHpfU2dOV045ak9ZAbERJaEdGTHpOVEIxd0hiUlo0TThDYVlRSnJNM3ViOFBtc2FBLUhfSVNkSm9IMjYySmtyOWZAGRlMyUVlobUVORUpKQVhYeE9WU3JzNQZDZD'
+var url = 'https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type&access_token=' + token
+
+const cardImage = document.getElementById('instaImage')
+const cardText = document.getElementById('instaCaption')
+
+function cardMount() {
+    fetch(url, {
+        method: 'GET',
+    })
+        .then(resp => resp.json())
+        .then(data => {
+
+            var card = document.getElementById('cardMount')
+
+            var feed = ''
+
+            for(let i = 0; i < data.data.length; i++) {
+                
+            }
+            data.data.slice(0, 8).forEach((element) => {
+                
+                    if (element.media_type === 'IMAGE' || element.media_type === 'CAROUSEL_ALBUM') {
+                        feed += `<div class="col-md-3">
+                                    <div class="card mb-3">
+                                        <img class="card-img-top" id="instaImage" src="${element.media_url}" alt="Card image cap">
+                                        <div class="card-body" >
+                                            <p class="card-text" id="instaCaption" style="color: #000; height: 120px; overflow-y: hidden; text-overflow: ellipsis">${element.caption}</p>
+                                        </div>
+                                    </div>
+                                </div>`
+                    }
+             console.log(element)
+            })
+            card.innerHTML = feed
+
+        })
+        .catch(err => console.log(err))
+}
+
+cardMount()
+
